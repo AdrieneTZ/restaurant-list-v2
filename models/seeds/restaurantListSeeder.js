6df138cc1977ab1d3c18cac11d50b89798f38f12
AdrieneTZ <tznali17@gmail.com>
2022-05-26 18:32:36 +0800

    feat: create data structure and add seed data to MongoDB

---
+++ models/seeds/restaurantListSeeder.js
@@ -0,0 +1,22 @@
+const ReataurantList = require('../restaurantList')
+const seedsData = require('./restaurantData.json')
+
+const db = require('../../config/mongoose')
+
+db.once('open', () => {
+  seedsData.results.forEach(seed => {
+    ReataurantList.create({
+      name: `${seed.name}`,
+      name_en: `${seed.name_en}`,
+      category: `${seed.category}`,
+      image: `${seed.image}`,
+      location: `${seed.location}`,
+      phone: `${seed.phone}`,
+      google_map: `${seed.google_map}`,
+      rating: `${seed.rating}`,
+      description: `${seed.description}`
+    })
+  })
+
+  console.log('restaurant Seed data created')
+})
