a2de515bd9a7df2523a7bd5df692ebba5add7373
AdrieneTZ <tznali17@gmail.com>
2022-06-18 12:35:07 +0800

    feat: create user register function and register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,4 +9,31 @@ router.get('/login', (req, res) => {
   return res.render('login')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  return res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return console.log('This email is already registered!')
+      } else {
+        User.create({ name, email, password })
+      }
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.get('/logout', (req, res) => {
+
+})
+
+
 module.exports = router
